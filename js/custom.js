f6665901abdb3b03d78bf05f5b284768a1d4c031
vasiliy_zakrhgevskyi <milan9111@ukr.net>
2021-01-20 11:25:02 +0200

    add slider

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   $('.slider').slick({
-    prevArrow:"<img class='a-left control-c prev slick-prev' src='./images/long-left-arrow.png'>",
-    nextArrow:"<img class='a-right control-c next slick-next' src='./images/long-right-arrow.png'>",
+    
     slidesToShow: 3,
     slidesToScroll: 3,
     speed: 1000,
