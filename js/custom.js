cb516c9c0d835f942dc82b6e8dfbd0cb33db5412
vasiliy_zakrhgevskyi <milan9111@ukr.net>
2021-01-21 17:10:02 +0200

    creature slider item

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,13 @@ $(document).ready(function(){
   $('.slider').slick({
     prevArrow:"<img class='a-left control-c prev slick-prev' src='./images/arrow-left.png'>",
     nextArrow:"<img class='a-right control-c next slick-next' src='./images/arrow-right.png'>",
-    
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1000,
+    easing: 'ease',
+    autoplay: true,
+    autoplaySpeed: 1000,
+    centerMode:true,
   });  
 });
 
